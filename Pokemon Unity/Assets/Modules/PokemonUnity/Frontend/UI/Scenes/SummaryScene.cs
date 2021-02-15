f08515e655b3510a1ecf3e06fa0d53280990fd54
Tomáš Stolárik <ponacko@gmail.com>
2021-02-16 00:32:56 +0100

    Make not implemented scenes print out that they are not implemented

--- Pokemon Unity/Assets/Modules/PokemonUnity/Frontend/UI/Scenes/SummaryScene.cs
+++ Pokemon Unity/Assets/Modules/PokemonUnity/Frontend/UI/Scenes/SummaryScene.cs
@@ -2,10 +2,20 @@
 /// Purpose: Summary UI (temporary) for Pokémon Unity frontend
 /// Author: IIColour_Spectrum
 /// Contributors: TeamPopplio
+
+using System;
+using System.Collections;
+using PokemonUnity.Backend.Serializables;
 using UnityEngine;
 using UnityEngine.UI;
 namespace PokemonUnity.Frontend.UI.Scenes {
 public class SummaryScene : BaseScene
 {
+    public IEnumerator control(Pokemon[] pokemonList, int currentPosition = 0, bool learning = false,
+        string newMoveString = null)
+    {
+        //TODO
+        yield return StartCoroutine(SceneScript.main.Dialog.DrawText("This menu has not yet been implemented."));
+    }
 }
 }
